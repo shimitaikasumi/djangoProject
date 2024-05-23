bd29a0c94dceb23ca8f1d8eda96335b76407a454
q3_kotsu <211056@std.hi-joho.ac.jp>
2024-05-23 12:21:05 +0900

    バックアップ

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'UTSUBASE',
+        'USER': 'q3_kotsu',
+        'PASSWORD': 'ko3q30',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
