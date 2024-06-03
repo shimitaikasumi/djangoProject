17a5f2756b2ecba65e46c67a550c4aea1e1dc182
q3_kotsu <211056@std.hi-joho.ac.jp>
2024-06-03 15:32:26 +0900

    バックアップ

--- Diripe/views.py
+++ Diripe/views.py
@@ -1,10 +1,61 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-
-def index(request):
-    return HttpResponse("ようこそ")
+from .models import Employee
 
 
 def login(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        empid = request.POST('empid')
+        emppasswd = request.POST('emppasswd')
+        try:
+            employee = Employee.objects.get(empid=empid)
+        except Employee.DoesNotExist:
+            messages.error(request,"利用者IDが違います")
+            return render(request,'login.html')
+
+        if employee.emppassed == emppasswd:
+            request.session['empid'] = employee.empid
+            if employee.emprole == 1:
+                return render(request,'admin_home.html')
+            elif employee.emprole == 2:
+                return render(request,'reception_home.html')
+            elif employee.emprole == 3:
+                return render(request,'doctor_home.html')
+        else:
+            messages.error(request, "パスワードが違います")
+            return render(request, 'login.html')
+    return render(request, 'login.html')
+
+def empregistration(request):
+    if request.method == 'POST':
+        empid = request.POST.get('empid')
+        empfname = request.POST.get('empfname')
+        emplname = request.POST.get('emplname')
+        emppasswd = request.POST.get('emppasswd')
+        emprole = request.POST.get('emprole')
+
+        if Employee.objects.filter(empid=empid).exists():
+            messages.error(request, 'このIDはすでに登録してあります。')
+        else:
+            employee = Employee(
+                empid=empid, empfname=empfname,
+                emplname=emplname,emppasswd=emppasswd,
+                emprole=emprole
+            )
+            employee.save()
+            messages.success(request, '従業員が登録されました。')
+            return redirect('')
+
+
+    return render(request, 'admin_registration.html', {'form': form})
+
+
+def employee_success(request):
+    return render(request, 'success.html')
+
+
+
+
+
